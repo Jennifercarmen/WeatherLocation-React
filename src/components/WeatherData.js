5104a9f41dcc7379cdf5a0e9f0f54a18d7c2b1c0
JenniferCarmen <jennifercarmens@gmail.com>
2018-04-30 18:39:35 -0500

    uso de componentes y constantes

---
+++ src/components/WeatherData.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WeatherTemperature from './WeatherTemperature';
+import WeatherExtraInfo from './WeatherExtraInfo';
+import {SNOW} from './../constants/weather';
+
+const WeatherData = () =>(
+    <div>
+    <WeatherTemperature temperature={20} weatherState={SNOW} />
+    <WeatherExtraInfo humidity={80} wind={'10m/s'}/>
+    </div>
+)
+export default WeatherData;
