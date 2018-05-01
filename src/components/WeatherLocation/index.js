ff02c0ed599264e53817a712996ca1716c635fa3
JenniferCarmen <jennifercarmens@gmail.com>
2018-05-01 10:34:28 -0500

    refactorizando codigo

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
+import {SUN} from './../../constants/weather';
 import './styles.css';
-
+const data = {
+    temperature:20,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '10m/s'
+}
 const WeatherLocation = () =>(
     <div className="weatherLocationCont"> 
         <Location city={'Buenos Aires'} />
-        <WeatherData />
+        <WeatherData data= {data} />
     </div>
 );
 
